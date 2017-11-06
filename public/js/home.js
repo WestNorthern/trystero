cbf8b90a329db8f28888b43a64eee09e12dc9d70
westnorthern <rrichardson610@gmail.com>
2017-11-06 11:54:55 -0700

    tweaked new post button by adding a toggle

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,16 @@
 $(function() {
 	$('#new-post').click(function(){
 		$('.new-post-box').show();
+		$('#nevermind').show();
+		$('#new-post').hide();
 	})
+
+		$('#nevermind').click(function(){
+		$('.new-post-box').hide();
+		$('#new-post').show();
+		$('#nevermind').hide();
+	})
+
 
 
 
